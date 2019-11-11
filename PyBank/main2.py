46c0ef99ff716ef50fe3f1c24ff51e534333bac2
carode <carode21944@gmail.com>
2019-11-10 22:37:54 -0500

    Adding comments

--- PyBank/main2.py
+++ PyBank/main2.py
@@ -1,7 +1,8 @@
-#modules
+#Formating how the code will print
 print("Financial Analysis")
 print("-------------------------------------------")
 
+#Importing dependencies
 import os 
 import csv
 
@@ -9,12 +10,14 @@ import csv
 # Set path for files
 csvpath = os.path.join('..', 'Resources', "budget_data.csv")
 
+#Setting Total_months, first_number, last_number, and diff to 0 and creating an empty list
 Total_months = 0 
 Net_Profit_loss = []
 first_number = 0 
 last_number = 0 
 diff = 0 
 #1
+#Reading csv
 with open (csvpath, newline = "") as csvfile:
     budget_dataCsv = csv.reader(csvfile, delimiter = ",")
     csv_header = next(csvfile)
@@ -29,10 +32,12 @@ print("Total months :" + str(len(budgetDatalist)))
 
 
 #2
+#Reading csv
 with open (csvpath) as csvfile: 
     reader = csv.reader(csvfile)
     header = next(reader)
-        
+    
+    #Iterating over each line, and adding 1 when the Total_months is 0, and appending to the empty list    
     for line in reader:
         if Total_months==0:
             Total_months+= 1
@@ -41,14 +46,16 @@ with open (csvpath) as csvfile:
 print("Net profit and loss: " + str(sum(Net_Profit_loss))+ " $")
 
 #3
+#Reading csv and creating empty lists
 with open (csvpath) as csvfile: 
     reader = csv.reader(csvfile)
     header = next(reader)
     prof_los = []
     diffs = []
+    #Iterating every line and append to the prof_los empty list
     for line in reader:
       prof_los.append(int(line[1]))
-    
+    #Creating try catch statements 
     for index, num in enumerate(prof_los):
       try:
        if index != 0:
@@ -56,17 +63,20 @@ with open (csvpath) as csvfile:
         diffs.append(diff)
       except Exception:
         continue
+        #Assigning the average of differences to a variable and printing 
     average = sum(diffs)/len(diffs)
     print("Average change:" + str(average))
 
 
 #3
+#Reading csv and creating empty lists
 with open (csvpath) as csvfile: 
     reader = csv.reader(csvfile)
     header = next(reader)
     prof_los = []
     months = []
     diffs = []
+    #Iterating over every line
     for line in reader:
       prof_los.append(int(line[1]))
       months.append(line[0])
